c67f242dcda51d73f2589c67655ba6f126dbdea2
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-06-02 12:08:02 +0700

    [chore]: first commit

---
+++ src/main/java/org/mini_lab/personal_cloud_sync/PersonalCloudSyncApplication.java
@@ -0,0 +1,13 @@
+package org.mini_lab.personal_cloud_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCloudSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalCloudSyncApplication.class, args);
+    }
+
+}
