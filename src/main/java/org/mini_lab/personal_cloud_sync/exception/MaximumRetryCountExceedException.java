b60c911ec7351673f63b099709ec503bed80efd1
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-06-05 12:29:02 +0700

    [business]: add MaximumRetryCountExceedException.java

---
+++ src/main/java/org/mini_lab/personal_cloud_sync/exception/MaximumRetryCountExceedException.java
@@ -0,0 +1,8 @@
+package org.mini_lab.personal_cloud_sync.exception;
+
+public class MaximumRetryCountExceedException extends RuntimeException {
+
+    public MaximumRetryCountExceedException() {
+        super("Maximum Retry Count Exceed should less or equal 5!");
+    }
+}
