1ca1d18e9a726dc4f8ff8baaf8349acc7d220adf
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-06-06 07:25:37 +0700

    [test]: add test case when estimate nextScheduledAt for Daily schedule type with proper runtime.

--- src/test/java/org/mini_lab/personal_cloud_sync/services/NextScheduledAtCalculationStrategyTest.java
+++ src/test/java/org/mini_lab/personal_cloud_sync/services/NextScheduledAtCalculationStrategyTest.java
@@ -143,4 +143,32 @@ class NextScheduledAtCalculationStrategyTest {
 
     }
 
+    @Test
+    void estimateNextScheduledAt_daily_shouldReturnTomorrow_whenRuntimeAlreadyPassed() {
+        Clock fixedClock = Clock.fixed(
+                Instant.parse("2026-06-06T08:00:00Z"),
+                ZoneOffset.UTC
+        );
+        NextScheduledAtRequest request = new NextScheduledAtRequest();
+        request.setScheduleType(ScheduleType.DAILY);
+        request.setRunTime(LocalTime.parse("06:00"));
+
+        assertEquals(OffsetDateTime.parse("2026-06-07T06:00:00Z"), NextScheduledAtCalculationStrategy.estimateNextScheduledAt(request, fixedClock).orElseThrow());
+
+    }
+
+    @Test
+    void estimateNextScheduledAt_daily_shouldReturnTodayRuntime_whenRuntimeNotPassed() {
+        Clock fixedClock = Clock.fixed(
+                Instant.parse("2026-06-06T08:00:00Z"),
+                ZoneOffset.UTC
+        );
+        NextScheduledAtRequest request = new NextScheduledAtRequest();
+        request.setScheduleType(ScheduleType.DAILY);
+        request.setRunTime(LocalTime.parse("10:00"));
+
+        assertEquals(OffsetDateTime.parse("2026-06-06T10:00:00Z"), NextScheduledAtCalculationStrategy.estimateNextScheduledAt(request, fixedClock).orElseThrow());
+
+    }
+
 }
