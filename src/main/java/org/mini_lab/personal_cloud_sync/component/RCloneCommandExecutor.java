ead498a65b212b2c8072c6a91349eae2bc89e62c
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-06-13 15:40:08 +0700

    [business]: add timeout for external process like Rclone and test cases.

--- src/main/java/org/mini_lab/personal_cloud_sync/component/RCloneCommandExecutor.java
+++ src/main/java/org/mini_lab/personal_cloud_sync/component/RCloneCommandExecutor.java
@@ -1,5 +1,7 @@
 package org.mini_lab.personal_cloud_sync.component;
 
+import lombok.RequiredArgsConstructor;
+import org.mini_lab.personal_cloud_sync.configuration.RCloneProperties;
 import org.mini_lab.personal_cloud_sync.dto.RCloneResult;
 import org.springframework.stereotype.Component;
 
@@ -8,23 +10,41 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class RCloneCommandExecutor {
+    private final RCloneProperties rCloneProperties;
+
     public RCloneResult executeCommand(List<String> command) throws IOException, InterruptedException {
         ProcessBuilder pb = new ProcessBuilder(command);
         pb.redirectErrorStream(true);
-        Process ps = pb.start();
-        InputStream inputStream = ps.getInputStream();
+        Process process = pb.start();
+        boolean finished = process.waitFor(rCloneProperties.getTimeOutSecond(), TimeUnit.SECONDS);
+
+        if (!finished) {
+            process.destroy();
+
+            if (!process.waitFor(rCloneProperties.getTimeOutSecond(), TimeUnit.SECONDS)) {
+                process.destroyForcibly();
+            }
+
+            return RCloneResult.builder()
+                    .exitCode(-1)
+                    .errorMessage("RClone process timed out")
+                    .build();
+        }
+
+        InputStream inputStream = process.getInputStream();
         String output;
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             output = br.lines().collect(Collectors.joining("\n"));
         }
-        int exitCode = ps.waitFor();
+        int exitCode = process.exitValue();
 
-        return RCloneResult
-                .builder()
+        return RCloneResult.builder()
                 .exitCode(exitCode)
                 .errorMessage(output)
                 .build();
--- src/test/java/org/mini_lab/personal_cloud_sync/executor/RCloneCommandExecutorTest.java
+++ src/test/java/org/mini_lab/personal_cloud_sync/executor/RCloneCommandExecutorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.mini_lab.personal_cloud_sync.component.RCloneCommandBuilder;
+import org.mini_lab.personal_cloud_sync.configuration.RCloneProperties;
 import org.mini_lab.personal_cloud_sync.dto.RCloneResult;
 import org.mini_lab.personal_cloud_sync.component.RCloneCommandExecutor;
 import org.mockito.InjectMocks;
@@ -15,8 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +27,9 @@ class RCloneCommandExecutorTest {
 
     @Mock
     RCloneCommandBuilder rCloneCommandBuilder;
+
+    @Mock
+    RCloneProperties rCloneProperties;
 
     @TempDir
     Path sourcePath;
@@ -41,6 +44,8 @@ class RCloneCommandExecutorTest {
                 .command(sourcePath.toString(), targetPath.toString()))
                 .thenReturn(List.of("rclone", "sync", sourcePath.toString(), targetPath.toString(), "--log-level", "INFO"));
 
+        when(rCloneProperties.getTimeOutSecond()).thenReturn(5);
+
         Path sourceFile = sourcePath.resolve("test.txt");
         Path targetFile = targetPath.resolve("test.txt");
         Files.writeString(sourceFile, "Hello World!");
@@ -53,5 +58,16 @@ class RCloneCommandExecutorTest {
 
     }
 
+    @Test
+    void executeCommand_whenCommandTimeout_shouldKillProcess()
+            throws IOException, InterruptedException {
 
+        when(rCloneProperties.getTimeOutSecond()).thenReturn(5);
+        RCloneResult result = rCloneCommandExecutor.executeCommand(
+                List.of("bash", "-c", "sleep 10")
+        );
+
+        assertEquals(-1, result.getExitCode());
+        assertEquals("RClone process timed out", result.getErrorMessage());
+    }
 }
