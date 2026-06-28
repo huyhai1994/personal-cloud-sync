1e438df8d42c2d0da75e5c6ed6dbfb898b74c253
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-06-28 12:52:33 +0700

    test: add heartbeat recording tests for running jobs

---
+++ src/test/java/org/mini_lab/personal_cloud_sync/services/SyncJobProcessorServiceIntegrationTest.java
@@ -0,0 +1,91 @@
+package org.mini_lab.personal_cloud_sync.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mini_lab.personal_cloud_sync.entities.SyncConfig;
+import org.mini_lab.personal_cloud_sync.entities.SyncJob;
+import org.mini_lab.personal_cloud_sync.enums.JobStatus;
+import org.mini_lab.personal_cloud_sync.enums.ScheduleType;
+import org.mini_lab.personal_cloud_sync.repositories.SyncAttemptRepository;
+import org.mini_lab.personal_cloud_sync.repositories.SyncConfigRepository;
+import org.mini_lab.personal_cloud_sync.repositories.SyncJobRepository;
+import org.mini_lab.personal_cloud_sync.support.AbstractIntegrationTest;
+import org.mini_lab.personal_cloud_sync.support.TestClockConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(TestClockConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SyncJobProcessorServiceIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    SyncJobProcessorService syncJobProcessorService;
+
+    @Autowired
+    private SyncJobRepository syncJobRepository;
+
+    @Autowired
+    private SyncConfigRepository syncConfigRepository;
+
+    @Autowired
+    private SyncAttemptRepository syncAttemptRepository;
+
+    @TempDir
+    Path sourcePath;
+
+    @TempDir
+    Path targetPath;
+
+    @AfterEach
+    void tearDown() {
+        syncAttemptRepository.deleteAllInBatch();
+        syncJobRepository.deleteAllInBatch();
+        syncConfigRepository.deleteAllInBatch();
+    }
+
+    @Test
+    void updateHeartbeat_whenJobStatusNotRunning_thenHeartBeatIsNull() {
+        SyncConfig syncConfig = saveSyncConfig("heart-beat-not-recorded");
+        SyncJob syncJob = saveSyncJob(syncConfig, JobStatus.PENDING);
+        Integer persistedSyncJobId = syncJob.getId();
+        assertDoesNotThrow(() -> syncJobProcessorService.updateHeartbeat(persistedSyncJobId));
+        SyncJob updatedHeartBeatSyncConfig = syncJobRepository.getSyncJobById(persistedSyncJobId).orElseThrow();
+        assertNull(updatedHeartBeatSyncConfig.getHeartBeatAt());
+    }
+
+    @Test
+    void updateHeartbeat_whenClaimJobCountNotZero_thenHeartbeatRecorded() {
+        SyncConfig syncConfig = saveSyncConfig("heart-beat-recorded");
+        SyncJob syncJob = saveSyncJob(syncConfig, JobStatus.RUNNING);
+        Integer persistedSyncJobId = syncJob.getId();
+        assertDoesNotThrow(() -> syncJobProcessorService.updateHeartbeat(persistedSyncJobId));
+        SyncJob updatedHeartBeatSyncConfig = syncJobRepository.getSyncJobById(persistedSyncJobId).orElseThrow();
+        assertNotNull(updatedHeartBeatSyncConfig.getHeartBeatAt());
+
+    }
+
+    private SyncConfig saveSyncConfig(String suffix) {
+        SyncConfig syncConfig = new SyncConfig();
+        syncConfig.setSourcePath(sourcePath.resolve(suffix).toString());
+        syncConfig.setTargetPath(targetPath.resolve(suffix).toString());
+        syncConfig.setScheduleType(ScheduleType.MANUAL);
+        return syncConfigRepository.saveAndFlush(syncConfig);
+    }
+
+    private SyncJob saveSyncJob(SyncConfig syncConfig, JobStatus status) {
+        SyncJob syncJob = new SyncJob();
+        syncJob.setSyncConfig(syncConfig);
+        syncJob.setFinalStatus(status);
+        return syncJobRepository.saveAndFlush(syncJob);
+    }
+}
